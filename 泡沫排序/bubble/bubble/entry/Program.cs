673bdcd701550b78bd35d245b57929713d863189
sean56787 <40943130@gm.nfu.edu.tw>
2025-07-11 17:01:06 +0800

    feature: init bubblesort

---
+++ 泡沫排序/bubble/bubble/entry/Program.cs
@@ -0,0 +1,14 @@
+﻿using sort.BubbleSort;
+
+namespace entry.program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] chars = { 7, 6, 5, 4, 3, 2, 1 };
+            BubbleSort b = new BubbleSort();
+            string anser = b.sort(b);
+        }
+    }
+}
---
+++ 泡沫排序/bubble/bubble/sort/BubbleSort.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sort.BubbleSort
+{
+    internal class BubbleSort
+    {
+        public string sort(int[] input)
+        {
+
+
+            return "fuck u";
+        }
+    }
+}
