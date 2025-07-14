4056b639c837bd487d7e87081bef56d529d22a5b
sean56787 <40943130@gm.nfu.edu.tw>
2025-07-14 18:51:55 +0800

    feature: add quick sort/insertion sort

---
+++ 快速排序/quick/quick/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    static void sort(int[] arr, int left, int right)
+    {
+        if (left >= right) return;
+
+        int pivot = arr[(left + right) / 2];
+        int index = partition(arr, left, right, pivot);
+        sort(arr, left, index - 1);
+        sort(arr, index, right);
+    }
+
+    static int partition(int[] arr, int left, int right,int pivot)
+    {
+        while(left <= right)
+        {
+            while (arr[left] < pivot) left++;
+            while (arr[right] > pivot) right--;
+
+            if (left <= right)
+            {
+                (arr[left], arr[right]) = (arr[right], arr[left]);
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+    static void Main(string[] args)
+    {
+        int[] arr = { 100, 5, 4, 3, 2, 1, -1, -99 };
+        sort(arr, 0, arr.Length-1);
+        Console.WriteLine(string.Join(" ", arr));
+    }
+}
---
+++ 插入排序/insertion/insertion/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    public static string sort(int[] input)
+    {
+        
+        int n = input.Length;
+        for(int i = 1; i < n; i++)
+        {
+            int index = input[i];
+            int j = i - 1;
+            while (j >= 0 && index < input[j])
+            {
+                input[j + 1] = input[j];
+                j--;
+            }
+            input[j+1] = index;
+        }
+
+        return String.Join(" ", input);
+    }
+
+    public void Swap(int[] arr, int a, int b)
+    {
+
+    }
+    public static void Main(string[] arg)
+    {
+        int[] arr = { 1,-1,2,-10,9,12,5,4,3,100 };
+        string anser = sort(arr);
+        Console.WriteLine(anser);
+    }
+}
