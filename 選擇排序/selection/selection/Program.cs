0bc990778f12bed0c2fa0816eaea6df9d9ed99c5
sean56787 <40943130@gm.nfu.edu.tw>
2025-07-14 13:42:46 +0800

    feature: selection sort complete

---
+++ 選擇排序/selection/selection/Program.cs
@@ -0,0 +1,13 @@
+﻿using selection.SelectionSort;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] input = { 7, 8, 6, 9, 1, 4, 0, 2, 3, 15, 999, -1 };
+        SelectionSort ss = new SelectionSort();
+
+        string anser = ss.sort(input);
+        Console.WriteLine(anser);
+    }
+}
---
+++ 選擇排序/selection/selection/SelectionSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selection.SelectionSort
+{
+    internal class SelectionSort
+    {
+        public string sort(int[] input)
+        {
+            int n = input.Length;
+            for(int i = 0; i < n; i++)
+            {
+                var min = i; //假設目前的是最小值
+                for(int j = i; j < n; j++)
+                {
+                    if (input[j] < input[min]) //遇到最小的紀錄起來
+                        min = j;
+                }
+                if(i!=min) // 最後與最小交換
+                    Swap(input, i, min);
+            }
+
+            return String.Join(" ", input);
+        }
+        public void Swap(int[] arr,int a,int b)
+        {
+            int tmp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = tmp;
+        }
+    }
+}
